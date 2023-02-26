b19b5dfad0cb2d216a27bdd366dad9863ad5cddd
tassilo <home@tassilo-egerter.de>
2023-02-26 13:54:10 +0100

    - added shell script

--- run_klipper_upload_service.sh
+++ run_klipper_upload_service.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
+# here goes the directory to monitor for changed gcode files
 dir_to_watch="/home/tassilo/3d_print/gcode/"
+# the ip adress of your hardware running Moonraker
 ip="192.168.1.165"
+# for nice red error messages in console
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
 echo "Klipper hardware's ip address is configured as $ip."
 echo "Upload service starting...."
 /home/tassilo/.local/share/virtualenvs/klipper_upload-cINr6ycz/bin/python /home/tassilo/programming/klipper_upload/gcode_upload_to_moonraker.py -d $dir_to_watch -i "$ip"
